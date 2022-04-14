1b6ec0f34f887f6b09713c450ef6d4b8f073ae45
Unnisha Deb <unnisha705@gmail.com>
2022-04-14 20:28:28 +0530

    Final

--- app/src/main/java/com/example/utop/Winner.java
+++ app/src/main/java/com/example/utop/Winner.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Winner extends AppCompatActivity {
     private TextView textView;
+    private LinearLayout layout;
+
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try{
+                Random rn = new Random();
+                layout.setBackgroundColor(Color.rgb(rn.nextInt()%255, rn.nextInt()%255, rn.nextInt()%255));
+                handler.postDelayed(runnable, 500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +43,8 @@ public class Winner extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.winner);
         textView.setText(player);
+
+        layout = (LinearLayout) findViewById(R.id.layout);
+        runnable.run();
     }
 }
